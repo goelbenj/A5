7901ee63889eec5c584d7a76c298ea7fd3efcc4e
goelbenj <ben_goel@rogers.com>
2023-11-28 23:42:26 -0500

    Add filename to arg

--- file_handling.cpp
+++ file_handling.cpp
@@ -3,10 +3,10 @@
 #include <fstream>
 #include <string>
 
-std::map<std::string, std::string> read_lines_from_file()
+std::map<std::string, std::string> read_lines_from_file(std::string filename)
 {
    std::map<std::string, std::string> map{};
-   std::ifstream inFile {"/Users/bengoel/Documents/A5/db1.txt"};
+   std::ifstream inFile {filename};
    while (inFile)
    {
        std::string key{}, value{};
@@ -24,7 +24,7 @@ std::map<std::string, std::string> read_lines_from_file()
 
 int main(int argc, char **argv)
 {
-    std::map<std::string, std::string> map = read_lines_from_file();
+    std::map<std::string, std::string> map = read_lines_from_file("/Users/bengoel/Documents/A5/db1.txt");
     for (const auto& elem : map)
     {
         std::cout << elem.first << " | " << elem.second << "\n";
